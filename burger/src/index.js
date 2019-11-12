5670516096e29fad4036e6133146bcbc5bbfa4c9
AntonioDelgadoSocialicity <antoniodelgado@socialicity.co.uk>
2019-11-12 08:58:31 +0000

    Fixing Firebase Config

--- burger/src/index.js
+++ burger/src/index.js
@@ -6,11 +6,32 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBEAsqDWQ9KfjXX2tki3xaIchTGhxhkpL0",
+    authDomain: "react-my-burger-9c1ab.firebaseapp.com",
+    databaseURL: "https://react-my-burger-9c1ab.firebaseio.com",
+    projectId: "react-my-burger-9c1ab",
+    storageBucket: "react-my-burger-9c1ab.appspot.com",
+    messagingSenderId: "614478310371",
+    appId: "1:614478310371:web:459f718a18e70793274ed6"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 const app = (
     <BrowserRouter>
         <App />
     </BrowserRouter>
 );
 
-ReactDOM.render( app, document.getElementById( 'root' ) );
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
